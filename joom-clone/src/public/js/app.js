21fd80d7205cb8ceef42a8e1a2c303aa36e5ced6
BrokenMental <commontoday@naver.com>
2021-12-01 18:47:09 +0900

    websocket 활용법 추가(_ws), socket.io 활용법 추가중

--- joom-clone/src/public/js/app.js
+++ joom-clone/src/public/js/app.js
@@ -1,59 +1,25 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
-const messageList = document.querySelector('ul');
-const nickForm = document.querySelector('#nick');
-const messageForm = document.querySelector('#message');
+const socket = io();
 
-//메시지 만들기
-function makeMessage(type, payload) {
-    //json 형식을 string으로 변환
-    return JSON.stringify({ type, payload });
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+
+function backendDone(msg) {
+    console.log('server is done!', msg);
 }
 
-//서버 시작 시
-socket.addEventListener('open', () => {
-    console.log('Connected to Server');
-});
-
-//메시지 받을 때
-socket.addEventListener('message', (message) => {
-    //console.log('New Message: ', message.data);
-    const li = document.createElement('li');
-    li.innerText = message.data;
-
-    messageList.append(li);
-});
-
-//서버 종료 시
-socket.addEventListener('close', () => {
-    console.log('Disconnected from Server');
-});
-
-/*
-setTimeout(() => {
-    //메시지 전송
-    socket.send('hello from the browser!');
-}, 10000);
-*/
-
-//메시지폼에서 전송 시
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
     event.preventDefault();
-
-    const input = messageForm.querySelector('input');
-    socket.send(makeMessage('new_message', input.value));
-    //socket.send(input.value);
-    input.value = '';
-}
-
-//닉네임폼에서 전송 시
-function handleNickSubmit(event) {
-    event.preventDefault();
-
-    const input = nickForm.querySelector('input');
-    socket.send(makeMessage('nickname', input.value));
+    const input = form.querySelector('input');
+    /*
+    emit event
+    1. 원하는 이름으로 데이터 포함 전송 가능(on으로 활용 시 이름이 같아야 함)
+    2. 데이터 전송, string 뿐 아니라 object도 가능
+    3. 콜백함수 전송 가능
+    4. 데이터는 무제한으로 입력 가능, 콜백함수는 맨 마지막에만 입력 가능
+    */
+    socket.emit('enter_room', { payload: input.value }, backendDone);
 
     input.value = '';
 }
 
-messageForm.addEventListener('submit', handleSubmit);
-nickForm.addEventListener('submit', handleNickSubmit);
+form.addEventListener('submit', handleRoomSubmit);
--- joom-clone/src/server.js
+++ joom-clone/src/server.js
@@ -1,6 +1,6 @@
 import http from 'http';
 import express from 'express';
-import { WebSocketServer } from 'ws';
+import SocketIO from 'socket.io';
 
 const app = express();
 
@@ -15,71 +15,22 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 //app.listen(3000, handleListen);
 
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
+const io = SocketIO(server);
 
-/*
-//연결 되었을 경우 실행 함수
-function handleConnection(socket) {
-    console.log(socket);
-}
-
-//연결 이벤트
-wss.on('connection', handleConnection);
-*/
-
-const sockets = [];
-
-wss.on('connection', (socket) => {
-    sockets.push(socket);
-    console.log('Connected to Browser');
-
-    socket['nickname'] = 'Anon';
-
-    //탭(페이지)이 닫혔을 때 실행
-    socket.on('close', () => {
-        console.log('Disconnected from the Browser');
+io.on('connection', socket => {
+    //console.log(socket);
+    /*
+    emit에서 설정 한 명칭과 동일한 이벤트를 실행
+    1. 데이터(여기선 msg)
+    2. 함수(여기선 done, done()과 같이 사용)
+    3. emit의 파라미터 개수와 같아야 함
+    */
+    socket.on('enter_room', (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done('hello from the backend');
+        }, 5000);
     });
-
-    //메시지를 받을 때
-    socket.on('message', (message) => {
-        //console.log(message.toString('utf8'));
-
-        //stringify를 사용해서 string 형으로 변환했던 json 내용을 다시 json 형태로 변경
-        const parsed = JSON.parse(message.toString('utf8'));
-        console.log(parsed, message.toString('utf8'));
-
-        //구분값 중 type이 new_message일 경우 실행
-        if (parsed.type === 'new_message') {
-            sockets.forEach(aSocket => {
-                aSocket.send(`${socket.nickname}: ${parsed.payload}`);
-            });
-            
-        } else if (parsed.type === 'nickname') {
-            //console.log(parsed.payload);
-            socket['nickname'] = parsed.payload;
-        }
-
-        /*
-        switch (parsed.type) {
-            case 'new_message':
-                sockets.forEach(aSocket => {
-                    aSocket.send(parsed.payload);
-                });
-
-                break;
-            
-            case 'nickname':
-                console.log(parsed.payload);
-
-                break;
-        }
-        */
-
-        //socket.send(message.toString('utf8'));
-    });
-
-    //메시지 전송
-    //socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
